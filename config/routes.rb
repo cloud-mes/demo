a3b80a4dfec14c0a66655b40f1956d6c93653ca8
Eric Guo <eric@cloud-mes.com>
2015-10-04 21:48:46 +0800

    Will mount modeler at root, which easier to test for a long time.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # This line mounts Cloud-MES's routes at the root of your application.
   # This means, any requests to URLs such as /modeler/factories, will go to Mes::Modeler::FactoriesController.
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
-  mount Mes::Modeler::Engine, :at => '/modeler'
+  mount Mes::Modeler::Engine, :at => '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
